e0db6566979a4e40b868ab736c868eb34a58dae8
Mostafa <mostafaqanbaryan@gmail.com>
2022-07-07 07:25:36 +0430

    Added: remove target files before linking them

--- init.sh
+++ init.sh
@@ -2,19 +2,26 @@
 dconf load / < ./keybindings.conf
 
 # Fish
+rm ~/.config/fish/functions/fish_prompt.fish
 ln -s ${PWD}/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+rm ~/.config/fish/config.fish
 ln -s ${PWD}/config.fish ~/.config/fish/config.fish
 
 # Vim
+rm ~/.vim -rf
 ln -s ${PWD}/vim ~/.vim
+rm ~/.vimrc
 ln -s ${PWD}/vimrc ~/.vimrc
 
 # NVim
+rm ~/.config/nvim -rf
 ln -s ${PWD}/nvim ~/.config/nvim
 
 # Starship
+rm ~/.config/starship.toml
 ln -s ${PWD}/starship.toml ~/.config/starship.toml
 
 # Wezterm
+rm ~/.wezterm.lua
 ln -s ${PWD}/wezterm.lua ~/.wezterm.lua
 cp ${PWD}/terminal.jpg ~/terminal.jpg
