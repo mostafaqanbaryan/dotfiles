3ebb47f9adc3d3bd69d75c2d9afede8016cb6681
Mostafa Qanbaryan <mostafaroboeq@gmail.com>
2022-11-19 09:57:34 +0330

    🐛 Hiding tab and showing it when creating a second tab is really slow

--- wezterm.lua
+++ wezterm.lua
@@ -2,12 +2,11 @@ local wezterm = require 'wezterm';
 local dimmer = {brightness=0.1};
 
 return {
-    window_decorations = "NONE",
     color_scheme = "Espresso",
     font = wezterm.font("Monego Ligatures"),
     font_size = 10.7,
     line_height = 1.3,
-    hide_tab_bar_if_only_one_tab = true,
+    hide_tab_bar_if_only_one_tab = false,
     warn_about_missing_glyphs = false,
     bidi_enabled = true,
     window_padding = {
@@ -16,11 +15,7 @@ return {
         top = 0,
         bottom = 0,
     },
-    window_background_image = "~/terminal.jpg",
-    window_background_image_hsb = {
-        brightness = 0.43,
-        hue = -0.5,
-    },
+    window_background_image = "/home/mostafaqanbaryan/terminal.jpg",
     inactive_pane_hsb = {
         hue = 0.5,
         saturation = 0.9,
@@ -28,15 +23,15 @@ return {
     },
 
     keys = {
-        {key="h", mods="META", action=wezterm.action{ActivatePaneDirection="Left"}},
-        {key="j", mods="META", action=wezterm.action{ActivatePaneDirection="Down"}},
-        {key="k", mods="META", action=wezterm.action{ActivatePaneDirection="Up"}},
-        {key="l", mods="META", action=wezterm.action{ActivatePaneDirection="Right"}},
-        {key="s", mods="META", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-        {key="v", mods="META", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-        {key="w", mods="META", action=wezterm.action{CloseCurrentPane={confirm=true}}},
-        {key="z", mods="META", action="TogglePaneZoomState"},
-        {key="r", mods="META", action=wezterm.action.PaneSelect{alphabet="qweasdzxc"}},
+        {key="h", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Left"}},
+        {key="j", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Down"}},
+        {key="k", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Up"}},
+        {key="l", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Right"}},
+        {key="s", mods="SUPER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        {key="v", mods="SUPER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        {key="w", mods="SUPER", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+        {key="z", mods="SUPER", action="TogglePaneZoomState"},
+        {key="r", mods="SUPER", action=wezterm.action.PaneSelect{alphabet="qweasdzxc"}},
         {key="F11", mods="", action="ToggleFullScreen"},
     },
 }
