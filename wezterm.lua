28569dc4bf2804140df9e6ef9498a9e606c3c9ce
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2023-04-28 10:46:40 +0330

    🔥 Now that I use Sway, I don't use splits in Wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -9,38 +9,6 @@ local pallete = {
     '#7C525D',
 }
 
-local function isViProcess(pane) 
-    -- get_foreground_process_name On Linux, macOS and Windows, 
-    -- the process can be queried to determine this path. Other operating systems 
-    -- (notably, FreeBSD and other unix systems) are not currently supported
-    return pane:get_foreground_process_name():find('n?vim') ~= nil
-end
-
-local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
-    if isViProcess(pane) then
-        window:perform_action(
-            -- This should match the keybinds you set in Neovim.
-            act.SendKey({ key = vim_direction, mods = 'CTRL' }),
-            pane
-        )
-    else
-        window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
-    end
-end
-
-wezterm.on('ActivatePaneDirection-right', function(window, pane)
-    conditionalActivatePane(window, pane, 'Right', 'l')
-end)
-wezterm.on('ActivatePaneDirection-left', function(window, pane)
-    conditionalActivatePane(window, pane, 'Left', 'h')
-end)
-wezterm.on('ActivatePaneDirection-up', function(window, pane)
-    conditionalActivatePane(window, pane, 'Up', 'k')
-end)
-wezterm.on('ActivatePaneDirection-down', function(window, pane)
-    conditionalActivatePane(window, pane, 'Down', 'j')
-end)
-
 wezterm.on('update-status', function(window, pane)
     local window_dims = window:get_dimensions()
     local is_big_window = window_dims.pixel_width > 700;
@@ -172,19 +140,6 @@ return {
         {key="j", mods="META", action=act.SendKey{ key = 'DownArrow' }},
         {key="k", mods="META", action=act.SendKey{ key = 'UpArrow' }},
         {key="l", mods="META", action=act.SendKey{ key = 'RightArrow' }},
-
-        {key='h', mods='CTRL', action=act.EmitEvent('ActivatePaneDirection-left')},
-        {key='j', mods='CTRL', action=act.EmitEvent('ActivatePaneDirection-down')},
-        {key='k', mods='CTRL', action=act.EmitEvent('ActivatePaneDirection-up')},
-        {key='l', mods='CTRL', action=act.EmitEvent('ActivatePaneDirection-right')},
-
-        {key="s", mods="SUPER", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
-        {key="v", mods="SUPER", action=act{
-            SplitPane={
-                direction="Down",
-                size={ Percent= 25 }
-            }
-        }},
 
         {key="w", mods="SUPER", action=act{CloseCurrentPane={confirm=true}}},
         {key="z", mods="SUPER", action="TogglePaneZoomState"},
