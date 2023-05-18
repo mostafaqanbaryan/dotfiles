5270cd3976d0cd81ba002a7dc29be0685a1e3309
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2023-05-18 20:27:32 +0330

    ✨ Better init.sh

--- fish/config.fish
+++ fish/config.fish
@@ -52,6 +52,7 @@ bind \e\[3\;5~ kill-word
 # Fun
 alias coffee 'termdown "3m" && sh -c "speaker-test -t sine -f 1000 -l 1 & sleep .5 && kill -9 \$!" 2>&1 > /dev/null'
 alias download 'aria2c -c -x 10 -s 10'
+alias kodi 'kodi --standalone --windowing=x11'
 
 ## SSH
 set -Ux GNOME_KEYRING_CONTROL /run/user/1000/keyring
--- init.sh
+++ init.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
-# dconf dump / > ./keybindings.conf.bak
-# dconf load / < ./keybindings.conf
-#
+
 sudo dnf install \
     swappy \
     wl-clipboard \
-    grim
+    grim \
+    wezterm \
+    fish \
+    starship \
+    neovim \
+    ranger \
+    sway \
+    waybar \
+    rofi \
+    highlight
 
-# Add ssh hostname to wezterm
-echo "
-    Host * !*git*
-        ServerAliveInterval 60
-        RequestTTY force
-        RemoteCommand printf \"\033Ptmux;\033\033]1337;SetUserVar=SSH_ENV=$(hostname | base64)\007\033\\\"; cat /run/motd.dynamic; $SHELL
-" >> ~/.ssh/config
 
 # Fish
-rm ~/.config/fish/functions -rf
-rm ~/.config/fish/completions -rf
-rm ~/.config/fish/config.fish
+rm ~/.config/fish/functions -rf 2>&1 /dev/null
+rm ~/.config/fish/completions -rf 2>&1 /dev/null
+rm ~/.config/fish/config.fish 2>&1 /dev/null
 ln -s ${PWD}/fish/completions ~/.config/fish/completions
 ln -s ${PWD}/fish/functions ~/.config/fish/functions
 ln -s ${PWD}/fish/config.fish ~/.config/fish/config.fish
 
 # NVim
-rm ~/.config/nvim -rf
+rm ~/.config/nvim -rf 2>&1 /dev/null
 ln -s ${PWD}/nvim ~/.config/nvim
 
 # Starship
-rm ~/.config/starship.toml
+rm ~/.config/starship.toml 2>&1 /dev/null
 ln -s ${PWD}/starship.toml ~/.config/starship.toml
 
 # Wezterm
-rm ~/.wezterm.lua
+rm ~/.wezterm.lua 2>&1 /dev/null
 ln -s ${PWD}/wezterm.lua ~/.wezterm.lua
 cp ${PWD}/terminal.jpg ~/terminal.jpg
 
 # Ranger
-rm ~/ranger.conf
+rm ~/.config/ranger/ranger.conf 2>&1 /dev/null
+mkdir ~/.config/ranger -p
 ln -s ${PWD}/ranger.conf ~/.config/ranger/ranger.conf
 
+# Fonts
+rm -rf ~/.fonts 2>&1 /dev/null
+ln -s ${PWD}/fonts ~/.fonts
+
 # Services
-ln -s ${PWD}/services ~/.config/systemd/user
+mkdir ~/.config/systemd -p
+ln -s ${PWD}/services/* ~/.config/systemd/user
 systemctl daemon-reload --user
 systemctl enable --now --user gnome-keyring-daemon
 
 # WM
-ln -s ${PWD}/sway/config ~/.config/sway/config
+rm -rf ~/.config/sway 2>&1 /dev/null
+ln -s ${PWD}/sway ~/.config/sway
+
+# Bar
+rm -rf ~/.config/waybar 2>&1 /dev/null
+ln -s ${PWD}/waybar ~/.config/waybar
