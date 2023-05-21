00f6736d68b6cf109561f7031c30e15076d74269
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2023-05-21 20:05:33 +0330

    ✨ Changed dnf to pacman and added new apps to init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-sudo dnf install \
-    swappy \
-    wl-clipboard \
-    grim \
-    wezterm \
-    fish \
-    starship \
-    neovim \
-    ranger \
-    sway \
-    waybar \
-    rofi \
-    highlight
-
-
 # Fish
+sudo pacman -S \
+    fish
 rm ~/.config/fish/functions -rf 2>&1 /dev/null
 rm ~/.config/fish/completions -rf 2>&1 /dev/null
 rm ~/.config/fish/config.fish 2>&1 /dev/null
@@ -24,37 +11,63 @@ ln -s ${PWD}/fish/functions ~/.config/fish/functions
 ln -s ${PWD}/fish/config.fish ~/.config/fish/config.fish
 
 # NVim
+sudo pacman -S \
+    neovim \
+    bat \
+    fzf
 rm ~/.config/nvim -rf 2>&1 /dev/null
 ln -s ${PWD}/nvim ~/.config/nvim
 
 # Starship
+sudo pacman -S \
+    starship
 rm ~/.config/starship.toml 2>&1 /dev/null
 ln -s ${PWD}/starship.toml ~/.config/starship.toml
 
 # Wezterm
+sudo pacman -S \
+    wezterm
 rm ~/.wezterm.lua 2>&1 /dev/null
 ln -s ${PWD}/wezterm.lua ~/.wezterm.lua
 cp ${PWD}/terminal.jpg ~/terminal.jpg
 
-# Ranger
+# Filemanager
+sudo pacman -S \
+    nautilus \
+    ranger
 rm ~/.config/ranger/ranger.conf 2>&1 /dev/null
 mkdir ~/.config/ranger -p
 ln -s ${PWD}/ranger.conf ~/.config/ranger/ranger.conf
+
+# WM
+sudo pacman -S \
+    wl-clipboard \
+    rofi \
+    highlight \
+    pass \
+    swappy \
+    grim \
+    slurp \
+    sway \
+    swaybg
+rm -rf ~/.config/sway 2>&1 /dev/null
+ln -s ${PWD}/sway ~/.config/sway
+
+# Bar
+sudo pacman -S \
+    waybar \
+    pavucontrol
+rm -rf ~/.config/waybar 2>&1 /dev/null
+ln -s ${PWD}/waybar ~/.config/waybar
 
 # Fonts
 rm -rf ~/.fonts 2>&1 /dev/null
 ln -s ${PWD}/fonts ~/.fonts
 
 # Services
+sudo pacman -S \
+    gnome-keyring
 mkdir ~/.config/systemd -p
 ln -s ${PWD}/services/* ~/.config/systemd/user
 systemctl daemon-reload --user
 systemctl enable --now --user gnome-keyring-daemon
-
-# WM
-rm -rf ~/.config/sway 2>&1 /dev/null
-ln -s ${PWD}/sway ~/.config/sway
-
-# Bar
-rm -rf ~/.config/waybar 2>&1 /dev/null
-ln -s ${PWD}/waybar ~/.config/waybar
