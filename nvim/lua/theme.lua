97d59d6b9a2627e8676d7aaa74491c7152e0881a
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-02-10 13:27:24 +0330

    ✨ Migrated to Lua and Lazy.nvim

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,3 +1,4 @@
+-- Theme
 require("tokyonight").setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
@@ -32,3 +33,24 @@ vim.cmd[[hi Folded guibg=NONE]]
 vim.cmd[[hi Folded guifg=#737aa2]]
 vim.cmd[[hi Folded ctermbg=NONE]]
 vim.cmd[[hi Search guifg=#ff6000]]
+
+-- Noice
+vim.notify = require("notify")
+require("noice").setup({
+    lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+        },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+})
+
