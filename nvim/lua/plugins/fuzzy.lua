5d2ecbd660774c9f19c23c3eb09b830f82eda638
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-03-03 07:40:27 +0330

    ✨ Added git repository as the root of fzf

--- nvim/lua/plugins/fuzzy.lua
+++ nvim/lua/plugins/fuzzy.lua
@@ -1,16 +1,24 @@
+getRoot = function()
+    local value = io.popen("git rev-parse --show-toplevel"):read("*a"):gsub("[\n\r]", "")
+    if not value then
+        value = '.'
+    end
+    return value
+end
+
 return {
     'ibhagwan/fzf-lua',
     branch = 'main',
     cmd = { 'CGFiles', 'CBuffers', 'CurrentDirFiles' },
     keys = {
-        { '<Leader>f',   '<cmd>lua require("fzf-lua").git_files()<CR>',                                                           { silent = true } },
-        { '<Leader>b',   '<cmd>lua require("fzf-lua").buffers()<CR>',                                                             { silent = true } },
-        { '<Leader>o',   '<cmd>lua require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h")})<CR>',                                { silent = true } },
-        { '<Leader>/iw', '<cmd>lua require("fzf-lua").grep_cword()<CR>',                                                          { silent = true } },
-        { '<Leader>/iW', '<cmd>lua require("fzf-lua").grep_cWORD()<CR>',                                                          { silent = true } },
-        { '<Leader>//',  '<cmd>lua require("fzf-lua").grep()<CR>',                                                                { silent = true } },
-        -- { '<Leader>//',  '<cmd>lua require("fzf-lua").grep_visual()<CR>',                                                         { silent = true, mode = 'v' } },
-        { '<F4>',        '<cmd>lua require("fzf-lua").lsp_code_actions({ winopts = { preview = { layout = "horizontal" }}})<CR>', { silent = true } },
+        { '<Leader>f',  '<cmd>lua require("fzf-lua").git_files()<CR>',                                                           { silent = true } },
+        { '<Leader>b',  '<cmd>lua require("fzf-lua").buffers()<CR>',                                                             { silent = true } },
+        { '<Leader>o',  '<cmd>lua require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h")})<CR>',                                { silent = true } },
+        { '<Leader>iw', '<cmd>lua require("fzf-lua").grep_cword({ cwd = getRoot()  })<CR>',                                      { silent = true } },
+        { '<Leader>iW', '<cmd>lua require("fzf-lua").grep_cWORD({ cwd = getRoot() })<CR>',                                       { silent = true } },
+        { '<Leader>/',  '<cmd>lua require("fzf-lua").grep({ cwd = getRoot() })<CR>',                                             { silent = true } },
+        { '<Leader>/',  '<cmd>lua require("fzf-lua").grep_visual({ cwd = getRoot() })<CR>',                                      { silent = true }, mode = 'v', },
+        { '<F4>',       '<cmd>lua require("fzf-lua").lsp_code_actions({ winopts = { preview = { layout = "horizontal" }}})<CR>', { silent = true } },
     },
     dependencies = {
         { 'nvim-tree/nvim-web-devicons' },
@@ -30,8 +38,8 @@ return {
                     vertical  = 'up:50%',
                 },
                 on_create = function()
-                    vim.keymap.set("t", "<C-k>", "<S-up>", { silent = true, buffer = true, remap = true })
-                    vim.keymap.set("t", "<C-j>", "<S-down>", { silent = true, buffer = true, remap = true })
+                    vim.keymap.set("t", "<C-u>", "<S-up>", { silent = true, buffer = true, remap = true })
+                    vim.keymap.set("t", "<C-d>", "<S-down>", { silent = true, buffer = true, remap = true })
                 end
             },
             keymap = {
