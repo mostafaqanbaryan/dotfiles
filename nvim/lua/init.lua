cd45f811db22c1616ea94f590da2eb676d97c3be
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-04-02 21:01:07 +0330

    ✨ Added nvim-dap for golang

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -6,28 +6,29 @@ vim.g.mapleader = ' '
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Faster startup
 require("lazy").setup({
-    { import = "plugins.lsp" },
-    { import = "plugins.git" },
-    { import = "plugins" },
+	{ import = "plugins.lsp" },
+	{ import = "plugins.dap" },
+	{ import = "plugins.git" },
+	{ import = "plugins" },
 }, {
-    defaults = {
-        lazy = true
-    },
-    checker = {
-        enabled = true,
-        notify = false
-    }
+	defaults = {
+		lazy = true
+	},
+	checker = {
+		enabled = true,
+		notify = false
+	}
 });
