b11f76edad6c17a8f0311b7a43a8dace5f9b5f4f
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-04-16 16:03:44 +0330

    🎨 Better colors for quickfix

--- nvim/lua/plugins/quickfix.lua
+++ nvim/lua/plugins/quickfix.lua
@@ -1,16 +1,19 @@
 return {
-    'kevinhwang91/nvim-bqf',
-    branch = 'main',
-    lazy = false,
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter'
-    },
-    config = function()
-        require('bqf').setup({
-            func_map = {
-                open = 'o',
-                openc = '<CR>'
-            }
-        })
-    end
+	'kevinhwang91/nvim-bqf',
+	branch = 'main',
+	lazy = false,
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter'
+	},
+	config = function()
+		require('bqf').setup({
+			func_map = {
+				open = 'o',
+				openc = '<CR>'
+			}
+		})
+		local palette = require('rose-pine.palette')
+		vim.api.nvim_set_hl(0, 'BqfPreviewBorder', { bg = palette.pine, fg = palette.base })
+		vim.api.nvim_set_hl(0, 'BqfPreviewTitle', { bg = palette.pine, fg = palette.base })
+	end
 }
