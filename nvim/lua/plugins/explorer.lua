8073d655e642a8e728dbb72c8d3ec2d506524bca
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-04-23 19:55:57 +0330

    🎨 Added sessions directory and fixed nvim explorer

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -1,116 +1,26 @@
--- Explorer
 return {
-    'kevinhwang91/rnvimr',
-    cmd = {
-        'RnvimrToggle'
-    },
-    keys = {
-        { "<leader>e", "<cmd>RnvimrToggle<CR>" },
-    },
+	'kevinhwang91/rnvimr',
+	cmd = {
+		'RnvimrToggle'
+	},
+	keys = {
+		{ "<leader>e", "<cmd>RnvimrToggle<CR>" },
+	},
+	init = function()
+		vim.g.rnvimr_enable_picker = true
+		vim.g.rnvimr_enable_ex = true
+		vim.g.rnvimr_draw_border = true
+		vim.g.rnvimr_enable_bw = true
+		vim.g.rnvimr_shadow_winblend = 40
+		vim.g.rnvimr_border_attr = { fg = 14, bg = -1 }
+		vim.cmd [[highlight link RnvimrNormal CursorLine]]
+		vim.g.rnvimr_layout = {
+			relative = 'editor',
+			height   = math.floor(vim.api.nvim_win_get_height(0) * 0.9),
+			width    = math.floor(vim.api.nvim_win_get_width(0) * 0.8),
+			row      = math.floor(vim.api.nvim_win_get_height(0) * 0.1),
+			col      = math.floor(vim.api.nvim_win_get_width(0) * 0.1),
+			style    = 'minimal',
+		}
+	end
 }
-
--- return {
---     'is0n/fm-nvim',
---     lazy = true,
---     dependencies = {
---         "nvim-lua/plenary.nvim",
---     },
---     cmd = {
---         'Neomutt',
---         'Lazygit',
---         'Joshuto',
---         'Ranger',
---         'Broot',
---         'Gitui',
---         'Xplr',
---         'Vifm',
---         'Skim',
---         'Nnn',
---         'Fff',
---         'Twf',
---         'Fzf',
---         'Fzy',
---         'Lf',
---         'Fm',
---         'TaskWarriorTUI',
---     },
---     keys = {
--- { "<leader>e", "<cmd>Xplr<CR>" },
---     },
---     config = function()
---         -- Set keymap so you can open the default terminal file manager (yazi)
---         vim.api.nvim_set_keymap()
---         require('fm-nvim').setup {
---             -- (Vim) Command used to open files
---             edit_cmd = "edit",
-
---             -- See `Q&A` for more info
---             on_close = {},
---             on_open = {},
-
---             -- UI Options
---             ui = {
---                 -- Default UI (can be "split" or "float")
---                 default = "float",
-
---                 float = {
---                     -- Floating window border (see ':h nvim_open_win')
---                     border    = "none",
-
---                     -- Highlight group for floating window/border (see ':h winhl')
---                     float_hl  = "Normal",
---                     border_hl = "FloatBorder",
-
---                     -- Floating Window Transparency (see ':h winblend')
---                     blend     = 0,
-
---                     -- Num from 0 - 1 for measurements
---                     height    = 0.8,
---                     width     = 0.8,
-
---                     -- X and Y Axis of Window
---                     x         = 0.5,
---                     y         = 0.5
---                 },
-
---                 split = {
---                     -- Direction of split
---                     direction = "topleft",
-
---                     -- Size of split
---                     size      = 24
---                 }
---             },
-
---             -- Terminal commands used w/ file manager (have to be in your $PATH)
---             cmds = {
---                 lf_cmd          = "lf", -- eg: lf_cmd = "lf -command 'set hidden'"
---                 fm_cmd          = "fm",
---                 nnn_cmd         = "nnn",
---                 fff_cmd         = "fff",
---                 twf_cmd         = "twf",
---                 fzf_cmd         = "fzf", -- eg: fzf_cmd = "fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
---                 fzy_cmd         = "find . | fzy",
---                 xplr_cmd        = "xplr",
---                 vifm_cmd        = "vifm",
---                 skim_cmd        = "sk",
---                 broot_cmd       = "broot",
---                 gitui_cmd       = "gitui",
---                 ranger_cmd      = "ranger",
---                 joshuto_cmd     = "joshuto",
---                 lazygit_cmd     = "lazygit",
---                 neomutt_cmd     = "neomutt",
---                 taskwarrior_cmd = "taskwarrior-tui"
---             },
-
---             -- Mappings used with the plugin
---             mappings = {
---                 vert_split = "<C-v>",
---                 horz_split = "<C-h>",
---                 tabedit    = "<C-t>",
---                 edit       = "<C-e>",
---                 ESC        = "<ESC>"
---             },
---         }
---     end,
--- }
