efd4f40d78ec39707b9ad652a349bb044d600f7a
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-04-25 11:54:07 +0330

    🎨 Converted pcal to a usable script in terminal

--- waybar/resources/shamsi.sh
+++ waybar/resources/shamsi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pcal=$HOME/.config/waybar/resources/./pcal.sh
+pcal=$HOME/.local/bin/pcal
 current=$($pcal -t)
 calendar=$($pcal -m)
 echo "{\"text\": \""$current"\", \"class\": \"custom-shamsi\", \"tooltip\": \"$calendar\"}" | sed 's/\s*\\n\s*\\n//'
