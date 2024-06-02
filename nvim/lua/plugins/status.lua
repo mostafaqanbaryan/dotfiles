8ec280fcc16baa4ebb98901d4056e1e959cc312c
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-06-02 09:41:22 +0330

    🔥 Noice

--- nvim/lua/plugins/status.lua
+++ nvim/lua/plugins/status.lua
@@ -203,13 +203,6 @@ return {
 			},
 		}
 
-		-- Add components to right sections
-		ins_right {
-			require("noice").api.statusline.mode.get,
-			cond = require("noice").api.statusline.mode.has,
-			color = { fg = "#ff9e64" },
-		}
-
 		ins_right { 'location', color = { fg = colors.orange, gui = 'bold' } }
 
 		-- ins_right {
