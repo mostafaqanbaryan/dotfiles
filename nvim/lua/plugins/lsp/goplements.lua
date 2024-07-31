112b44906f4de2534e158e369511ec52b3db5443
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-07-31 11:19:53 +0330

    ✨ Show go interface implements

---
+++ nvim/lua/plugins/lsp/goplements.lua
@@ -0,0 +1,9 @@
+return {
+    "maxandron/goplements.nvim",
+    ft = "go",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+}
