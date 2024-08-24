038b058b8cb0b00ab72841ba5d8f388cbb424e77
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-08-24 09:23:49 +0330

    🐛 Fixed persistence directory

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -2,6 +2,6 @@ return {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
     opts = {
-        -- add any custom options here
+        dir = vim.fn.stdpath("state") .. "/sessions/"
     }
 }
--- nvim/lua/plugins/startify.lua
+++ nvim/lua/plugins/startify.lua
@@ -1,55 +1,55 @@
 return {
-	'mhinz/vim-startify',
-	lazy = false,
-	branch = 'master',
-	config = function()
-		-- Start from 1
-		vim.g.startify_custom_indices      = vim.cmd("map(range(1,100), 'string(v:val)')")
-		vim.g.startify_session_sort        = 1
-		vim.g.startify_session_number      = 10
-		vim.g.startify_session_persistence = 1
-		vim.g.startify_session_autoload    = 1
-		vim.g.startify_session_dir         = '~/.config/nvim/sessions'
-		vim.g.startify_relative_path       = 0
-		vim.g.startify_change_to_dir       = 0
+    'mhinz/vim-startify',
+    lazy = false,
+    branch = 'master',
+    config = function()
+        -- Start from 1
+        vim.g.startify_custom_indices      = vim.cmd("map(range(1,100), 'string(v:val)')")
+        vim.g.startify_session_sort        = 1
+        vim.g.startify_session_number      = 10
+        vim.g.startify_session_persistence = 1
+        vim.g.startify_session_autoload    = 1
+        vim.g.startify_session_dir         = vim.fn.stdpath("state") .. "/sessions/"
+        vim.g.startify_relative_path       = 0
+        vim.g.startify_change_to_dir       = 0
 
-		vim.g.startify_commands            = {
-			{ c = { 'Open Configs', ':Config' } }
-		}
-		vim.g.startify_lists               = {
-			{ type = 'commands',  header = { '   [Commands]' } },
-			{ type = 'files',     header = { '   [Files]' } },
-			{ type = 'dir',       header = { '   [Directory] ' .. vim.fn.getcwd() } },
-			{ type = 'sessions',  header = { '   [Sessions]' } },
-			{ type = 'bookmarks', header = { '   [Bookmarks]' } },
-		}
-		vim.g.startify_custom_header       = {
-			"                                                                                                    ",
-			"                                                                                                    ",
-			"                                                                                                    ",
-			"                         .^!^                                           .!~:                        ",
-			"                    ^!JPB&B7.                                            !&&BPJ!:                   ",
-			"                ^?P#@@@@@G.                   :       :                   ~@@@@@&B5!:               ",
-			"             ^JB@@@@@@@@@7                   .#~     ?G                   .&@@@@@@@@&G?:            ",
-			"          .7G@@@@@@@@@@@@#!                  7@&^   ~@@^                 .5@@@@@@@@@@@@@G7.         ",
-			"        .?#@@@@@@@@@@@@@@@@BY!^.             B@@&BBB&@@Y              :~Y#@@@@@@@@@@@@@@@@#?.       ",
-			"       !#@@@@@@@@@@@@@@@@@@@@@@#G5Y?!~^:..  ~@@@@@@@@@@#.   ..::^!7J5B&@@@@@@@@@@@@@@@@@@@@@#!      ",
-			"     .5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&##B#@@@@@@@@@@@BBBB##&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y     ",
-			"    :B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5    ",
-			"   .B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@J   ",
-			"   5@&#BGGPPPPPGGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BGGP555PPGBB#&#:  ",
-			"   ^:.            .^!YB@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&57^.            .:^.  ",
-			"                       ~G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5:                      ",
-			"                         P@@@#BGGGGB#@@@@@@@@@@@@@@@@@@@@@@@@@#BP5555PG#@@@P                        ",
-			"                         :J!:.      .^!JG&@@@@@@@@@@@@@@@@#57^.        .:!5~                        ",
-			"                                         :?G@@@@@@@@@@@@P!.                                         ",
-			"                                            ~5@@@@@@@@5^                                            ",
-			"                                              ^P@@@@G^                                              ",
-			"                                                !#@?                                                ",
-			"                                                 :^                                                 ",
-			"                                                                                                    ",
-			"                                                                                                    ",
-			"                                                                                                    ",
-		}
-	end
+        vim.g.startify_commands            = {
+            { c = { 'Open Configs', ':Config' } }
+        }
+        vim.g.startify_lists               = {
+            { type = 'commands',  header = { '   [Commands]' } },
+            { type = 'files',     header = { '   [Files]' } },
+            { type = 'dir',       header = { '   [Directory] ' .. vim.fn.getcwd() } },
+            { type = 'sessions',  header = { '   [Sessions]' } },
+            { type = 'bookmarks', header = { '   [Bookmarks]' } },
+        }
+        vim.g.startify_custom_header       = {
+            "                                                                                                    ",
+            "                                                                                                    ",
+            "                                                                                                    ",
+            "                         .^!^                                           .!~:                        ",
+            "                    ^!JPB&B7.                                            !&&BPJ!:                   ",
+            "                ^?P#@@@@@G.                   :       :                   ~@@@@@&B5!:               ",
+            "             ^JB@@@@@@@@@7                   .#~     ?G                   .&@@@@@@@@&G?:            ",
+            "          .7G@@@@@@@@@@@@#!                  7@&^   ~@@^                 .5@@@@@@@@@@@@@G7.         ",
+            "        .?#@@@@@@@@@@@@@@@@BY!^.             B@@&BBB&@@Y              :~Y#@@@@@@@@@@@@@@@@#?.       ",
+            "       !#@@@@@@@@@@@@@@@@@@@@@@#G5Y?!~^:..  ~@@@@@@@@@@#.   ..::^!7J5B&@@@@@@@@@@@@@@@@@@@@@#!      ",
+            "     .5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&##B#@@@@@@@@@@@BBBB##&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y     ",
+            "    :B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5    ",
+            "   .B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@J   ",
+            "   5@&#BGGPPPPPGGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BGGP555PPGBB#&#:  ",
+            "   ^:.            .^!YB@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&57^.            .:^.  ",
+            "                       ~G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5:                      ",
+            "                         P@@@#BGGGGB#@@@@@@@@@@@@@@@@@@@@@@@@@#BP5555PG#@@@P                        ",
+            "                         :J!:.      .^!JG&@@@@@@@@@@@@@@@@#57^.        .:!5~                        ",
+            "                                         :?G@@@@@@@@@@@@P!.                                         ",
+            "                                            ~5@@@@@@@@5^                                            ",
+            "                                              ^P@@@@G^                                              ",
+            "                                                !#@?                                                ",
+            "                                                 :^                                                 ",
+            "                                                                                                    ",
+            "                                                                                                    ",
+            "                                                                                                    ",
+        }
+    end
 }
