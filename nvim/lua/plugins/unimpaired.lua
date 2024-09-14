6bab7295ea9c5e15ac7210df9d5870af5292dbba
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-09-14 09:21:35 +0330

    ✨ Added go to error to unimpaired

--- nvim/lua/plugins/unimpaired.lua
+++ nvim/lua/plugins/unimpaired.lua
@@ -1,1 +1,13 @@
-return { 'tpope/vim-unimpaired', branch = 'master', lazy = false }
+return {
+    'tpope/vim-unimpaired',
+    branch = 'master',
+    lazy = false,
+    init = function()
+        vim.keymap.set('n', ']D',
+            '<cmd>lua vim.diagnostic.goto_next( {severity=vim.diagnostic.severity.ERROR, wrap = true} )<CR>',
+            { silent = true })
+        vim.keymap.set('n', '[D',
+            '<cmd>lua vim.diagnostic.goto_prev( {severity=vim.diagnostic.severity.ERROR, wrap = true} )<CR>',
+            { silent = true })
+    end
+}
