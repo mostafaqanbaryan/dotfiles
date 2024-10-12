4e25bc10c9e0e827cac506cef761081b48932a88
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-10-12 14:49:50 +0330

    🐛 Fixed indentation of yaml files

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -12,5 +12,16 @@ return {
         local palette = require('rose-pine.palette')
         vim.cmd.highlight('IndentLine guifg=#413f4f') -- palette.muted with a little transparency
         vim.cmd.highlight('IndentLineCurrent guifg=' .. palette.muted)
+
+
+        -- Set indentation for *.yaml files
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+            pattern = { "*.yaml", "*.yml", "*.json" },
+            callback = function()
+                vim.opt_local.softtabstop = 2
+                vim.opt_local.tabstop = 2
+                vim.opt_local.shiftwidth = 2
+            end
+        })
     end,
 }
