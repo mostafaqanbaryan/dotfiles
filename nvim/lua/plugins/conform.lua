3e44b0e0d723da9c64ddf9ce37f956b3614ab3b2
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2024-11-04 09:54:03 +0330

    🐛 Increased prettier timeout

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -42,7 +42,7 @@ return {
                 if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
                     return
                 end
-                return { timeout_ms = 1500, lsp_fallback = true }
+                return { timeout_ms = 5000, lsp_fallback = true }
             end,
         })
 
