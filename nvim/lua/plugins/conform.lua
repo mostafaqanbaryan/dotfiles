cb4df18c4fbcaa73661bed1cbcc39426454f46f8
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2025-01-08 08:51:27 +0330

    🐛 Fixed window rules

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -18,13 +18,13 @@ return {
         -- Define your formatters
         formatters_by_ft = {
             lua = { "stylua" },
-            scss = { "prettierd", "prettier", stop_after_first = true },
+            scss = { "prettier", stop_after_first = true },
             python = { "isort", "black", stop_after_first = true },
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-            typescript = { "prettierd", "prettier", stop_after_first = true },
-            typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-            javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-            php = { "prettierd", "prettier", stop_after_first = true },
+            javascript = { "prettier", stop_after_first = true },
+            typescript = { "prettier", stop_after_first = true },
+            typescriptreact = { "prettier", stop_after_first = true },
+            javascriptreact = { "prettier", stop_after_first = true },
+            php = { "prettier", stop_after_first = true },
         },
         -- Customize formatters
         formatters = {
