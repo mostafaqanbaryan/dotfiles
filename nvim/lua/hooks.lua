c219a8bcc7da18147944bd1dc26de796307ae0c4
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2025-01-22 06:59:35 +0330

    ✨ Added toggle search on moving cursor

--- nvim/lua/hooks.lua
+++ nvim/lua/hooks.lua
@@ -10,3 +10,30 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, {
         end
     end
 })
+
+
+-- Toggle search highlight on moving cursor
+vim.api.nvim_create_autocmd("InsertEnter", {
+    group = vim.api.nvim_create_augroup("ToggleSearchHighlight", { clear = true }),
+    callback = function()
+        vim.schedule(function() vim.cmd("nohlsearch") end)
+    end
+})
+vim.api.nvim_create_autocmd("CursorMoved", {
+    group = vim.api.nvim_create_augroup("ToggleSearchHighlight", { clear = true }),
+    callback = function()
+        local view, rpos = vim.fn.winsaveview(), vim.fn.getpos(".")
+        vim.cmd(string.format("silent! keepjumps go%s",
+            (vim.fn.line2byte(view.lnum) + view.col + 1 - (vim.v.searchforward == 1 and 2 or 0))))
+        local ok, _ = pcall(vim.cmd, "silent! keepjumps norm! n")
+        local insearch = ok and (function()
+            local npos = vim.fn.getpos(".")
+            return npos[2] == rpos[2] and npos[3] == rpos[3]
+        end)()
+        -- restore original view and position
+        vim.fn.winrestview(view)
+        if not insearch then
+            vim.schedule(function() vim.cmd("nohlsearch") end)
+        end
+    end
+})
