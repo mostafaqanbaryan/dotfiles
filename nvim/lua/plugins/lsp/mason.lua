f8ea0ce074f6d46da3b577cfb641a2b877aaad88
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2025-03-06 12:34:52 +0330

    ✨ Added installer in mason for formatters and linters

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,5 +1,35 @@
 return {
     'williamboman/mason.nvim',
     lazy = false,
-    config = true,
+    dependencies = {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+
+        local mason_tool_installer = require("mason-tool-installer")
+
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "staticcheck",       -- go linter
+                "prettierd",         -- prettier formatter
+                "stylua",            -- lua formatter
+                "gofumpt",           -- go formatter
+                "goimports",         -- go formatter
+                "goimports-reviser", -- go formatter
+                "eslint_d",          -- js linter
+            },
+        })
+    end
 }
