15e668c6fc556fba297d087fd89287353bb5e122
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2025-04-08 17:24:38 +0330

    ✨ Custom logic for php projects

--- nvim/lua/plugins/lsp/lint.lua
+++ nvim/lua/plugins/lsp/lint.lua
@@ -10,18 +10,36 @@ return {
 		lint.linters_by_ft = {
 			php = { "phpstan" },
 		}
+		local phpstan = require("lint").linters.phpstan
+		phpstan.args = {
+			"analyze",
+			"--error-format=json",
+			"--no-progress",
+			"--memory-limit=2G",
+		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
-				lint.try_lint()
+				if vim.bo.filetype == "php" then
+					-- If project exists in a subdirectory, we have to go to the subdirectory and run linter
+					local dirs = { "backend", "html", "app", "." }
+					for i, x in pairs(dirs) do
+						local f = io.open(x .. "/phpstan.neon", "r")
+						if f ~= nil then
+							f.close()
+							vim.cmd("cd " .. x)
+							lint.try_lint()
+							vim.cmd("cd ..")
+							break
+						end
+					end
+				else
+					lint.try_lint()
+				end
 			end,
 		})
-
-		vim.keymap.set("n", "<leader>l", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
 	end,
 }
