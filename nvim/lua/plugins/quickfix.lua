bfb117575f1a29a50b88ed0333ac047363314f60
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2025-04-11 17:56:31 +0330

    🎨 Better config for quickfix

--- nvim/lua/plugins/quickfix.lua
+++ nvim/lua/plugins/quickfix.lua
@@ -1,19 +1,21 @@
 return {
-    'kevinhwang91/nvim-bqf',
-    branch = 'main',
-    event = 'VeryLazy',
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter'
-    },
-    config = function()
-        require('bqf').setup({
-            func_map = {
-                open = 'o',
-                openc = '<CR>'
-            }
-        })
-        local palette = require('rose-pine.palette')
-        vim.api.nvim_set_hl(0, 'BqfPreviewBorder', { bg = palette.pine, fg = palette.base })
-        vim.api.nvim_set_hl(0, 'BqfPreviewTitle', { bg = palette.pine, fg = palette.base })
-    end
+	"kevinhwang91/nvim-bqf",
+	branch = "main",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("bqf").setup({
+			preview = {
+				win_height = 9999,
+			},
+			func_map = {
+				open = "o",
+				openc = "<CR>",
+				pscrollup = "<C-u>",
+				pscrolldown = "<C-d>",
+			},
+		})
+	end,
 }
