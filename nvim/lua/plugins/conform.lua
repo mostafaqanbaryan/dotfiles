9ecf8b51aefba4388ecc701c4954f194f225cb90
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2025-09-02 17:41:29 +0330

    ✨ Added ts/js/go auto import

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,18 +1,7 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
-	cmd = { "ConformInfo", "ConformDisable", "ConformEnable" },
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<F3>",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
+	cmd = { "ConformInfo" },
 	-- Everything in opts will be passed to setup()
 	opts = {
 		-- Define your formatters
@@ -34,36 +23,9 @@ return {
 		},
 	},
 	init = function()
-		vim.g.disable_autoformat = false
-		vim.b.disable_autoformat = false
-		require("conform").setup({
-			format_on_save = function(bufnr)
-				-- Disable with a global or buffer-local variable
-				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-					return
-				end
-				return { timeout_ms = 5000, lsp_fallback = true }
-			end,
-		})
+		require("conform").setup()
 
 		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		vim.api.nvim_create_user_command("ConformDisable", function(args)
-			if args.bang then
-				-- FormatDisable! will disable formatting just for this buffer
-				vim.b.disable_autoformat = true
-			else
-				vim.g.disable_autoformat = true
-			end
-		end, {
-			desc = "Disable autoformat-on-save",
-			bang = true,
-		})
-		vim.api.nvim_create_user_command("ConformEnable", function()
-			vim.b.disable_autoformat = false
-			vim.g.disable_autoformat = false
-		end, {
-			desc = "Re-enable autoformat-on-save",
-		})
 	end,
 }
--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,3 +1,78 @@
+local function setup_format_on_save(args)
+	vim.g.disable_autoformat = true
+
+	local formatter_group = vim.api.nvim_create_augroup("formatter_au", { clear = true })
+
+	for _, name in ipairs({
+		"source.addMissingImports.ts",
+		"source.removeUnused.ts",
+		-- "source.organizeImports",
+		-- "source.fixAll.eslint",
+	}) do
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
+			group = formatter_group,
+			callback = function()
+				if vim.g.disable_autoformat then
+					return
+				end
+				vim.lsp.buf.code_action({ apply = true, context = { only = { name }, diagnostics = {} } })
+			end,
+		})
+	end
+
+	-- Autoimport and sort imports in go files
+	-- https://go.dev/gopls/editor/vim#neovim-imports
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		group = formatter_group,
+		callback = function()
+			if vim.g.disable_autoformat then
+				return
+			end
+			local params = vim.lsp.util.make_range_params()
+			params.context = { only = { "source.organizeImports" } }
+			-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+			-- machine and codebase, you may want longer. Add an additional
+			-- argument after params if you find that you have to write the file
+			-- twice for changes to be saved.
+			-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+			for cid, res in pairs(result or {}) do
+				for _, r in pairs(res.result or {}) do
+					if r.edit then
+						local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+						vim.lsp.util.apply_workspace_edit(r.edit, enc)
+					end
+				end
+			end
+		end,
+	})
+
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*",
+		group = formatter_group,
+		callback = function()
+			if vim.g.disable_autoformat then
+				return
+			end
+			require("conform").format({ bufnr = args.buf, timeout_ms = 5000 })
+		end,
+	})
+
+	vim.api.nvim_create_user_command("FormatterDisable", function(args)
+		vim.g.disable_autoformat = true
+	end, {
+		desc = "Disable autoformat-on-save",
+	})
+
+	vim.api.nvim_create_user_command("FormatterEnable", function(args)
+		vim.g.disable_autoformat = false
+	end, {
+		desc = "Enable autoformat-on-save",
+	})
+end
+
 return {
 	"neovim/nvim-lspconfig",
 	lazy = false,
@@ -90,6 +165,10 @@ return {
 				if client ~= nil and client:supports_method("textDocument/definition") then
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 				end
+
+				vim.lsp.on_type_formatting.enable()
+
+				setup_format_on_save(args)
 
 				vim.keymap.set(
 					"n",
