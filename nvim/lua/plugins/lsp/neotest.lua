894ad52fa941f663d540ce4a7ff4e506b6ac50e1
Mostafa Qanbaryan <mostafaqanbaryan.ic@gmail.com>
2026-04-21 17:05:44 +0330

    feat: add keymaps for running tests

--- nvim/lua/plugins/lsp/neotest.lua
+++ nvim/lua/plugins/lsp/neotest.lua
@@ -10,6 +10,14 @@ return {
 	},
 	config = function()
 		require("neotest").setup({
+			config = {
+				status = {
+					enabled = true,
+					signs = true,
+					virtual_text = true,
+				},
+			},
+
 			adapters = {
 				require("neotest-golang")(),
 				require("neotest-phpunit")({
@@ -27,5 +35,36 @@ return {
 			local name = vim.fn.input("Enter test container name: ")
 			vim.g.test_container = name
 		end, {})
+
+		vim.keymap.set("n", "<F8>", function()
+			require("neotest").watch.toggle(vim.fn.expand("%"))
+			require("neotest").summary.toggle()
+		end)
+
+		vim.keymap.set("n", "<F9>", function()
+			require("neotest").run.run()
+			require("neotest").summary.open()
+		end)
+
+		vim.keymap.set("n", "<F10>", function()
+			require("neotest").run.run(vim.fn.expand("%"))
+			require("neotest").summary.open()
+		end)
+
+		vim.keymap.set("n", "[t", function()
+			require("neotest").jump.prev()
+		end)
+
+		vim.keymap.set("n", "]t", function()
+			require("neotest").jump.next()
+		end)
+
+		vim.keymap.set("n", "[T", function()
+			require("neotest").jump.prev({ status = "failed" })
+		end)
+
+		vim.keymap.set("n", "]T", function()
+			require("neotest").jump.next({ status = "failed" })
+		end)
 	end,
 }
